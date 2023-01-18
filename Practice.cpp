59225b3df76a49d8e7a4bd109aed98fe43e2fcb2
blaze <blaze@LAPTOP-3PFIDF6J>
2023-01-17 20:20:34 -0600

    Add project files.

---
+++ Practice.cpp
@@ -0,0 +1,17 @@
+// Blaze Snider
+// Data Structures
+// 01/17/23
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name1, name2;
+
+    cout << "Enter Your First and Last Name: ";
+    cin >> name1 >> name2;
+    cout << endl;
+    cout << "Hello there " << name1 << " " << name2 << " nice to meet you!" << endl;
+}
